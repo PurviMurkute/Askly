6540f5ffe0bd54390ef476b79b19545d70df2d91
PurviMurkute <purvimurkute51@gmail.com>
2025-07-10 14:35:53 +0530

    creating gemini clone

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import {
+  Menu,
+  Plus,
+  MessageSquareMore,
+  BadgeQuestionMark,
+  Settings,
+  Power,
+} from "lucide-react";
+
+const SideBar = () => {
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+  const toggleSideBar = () => {
+    setIsSideBarOpen(!isSideBarOpen);
+  };
+
+  return (
+    <div className={`bg-gray-200 flex flex-col min-h-screen justify-between items-center py-3 ${isSideBarOpen ? 'w-36' : 'w-16'} transition-width duration-300`}>
+      <div className="flex flex-col space-y-5">
+        <button onClick={toggleSideBar} className="cursor-pointer"><Menu /></button>
+        <button className="flex justify-center items-center p-2 bg-gray-400 text-white rounded-full hover:bg-gray-600 transition-colors duration-200">
+          <Plus/>{isSideBarOpen?<span>New Chat </span> : null}
+        </button>
+        <div>
+          {isSideBarOpen?<h3 className="mb-2">Recents</h3> : null}
+          <p className="flex items-center space-x-2 cursor-pointer">
+            <MessageSquareMore color="gray" className="w-5" />
+            {isSideBarOpen? <span>Message 1...</span> : null}
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col space-y-4">
+        <button className="flex items-center space-x-2 cursor-pointer">
+          <BadgeQuestionMark />
+          {isSideBarOpen ? <span>Help</span> : null}
+        </button>
+        <button className="flex items-center space-x-2 cursor-pointer">
+          <Settings />
+          {isSideBarOpen ? <span>Settings</span> : null}
+        </button>
+        <button className="flex items-center space-x-2 cursor-pointer">
+          <Power />
+          {isSideBarOpen ? <span>Sign Out</span> : null}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
